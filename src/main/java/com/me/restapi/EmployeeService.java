4a5d01cdf2ee46ca12e81415e2efe32282167bd6
brianchanoocl <Delubdi118>
2021-12-08 15:11:08 +0800

    [restapi] [Brian] -feat: Implemented code for should_return_all_employees_when_find_all_given_employees()

--- src/main/java/com/me/restapi/EmployeeService.java
+++ src/main/java/com/me/restapi/EmployeeService.java
@@ -1,10 +1,19 @@
 package com.me.restapi;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     public List<Employee> findAll() {
-        return null;
+        return employeeRepository.findAll();
     }
 }
